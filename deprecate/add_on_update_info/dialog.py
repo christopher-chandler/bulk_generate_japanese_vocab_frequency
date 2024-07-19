402454f984103239375fb3741a364fe275097c61
Christopher Chandler <johndoe@example.com>
2024-07-19 22:55:20 +0200

    remove unused code

--- deprecate/add_on_update_info/dialog.py
+++
@@ -1,9 +0,0 @@
-from PyQt6 import QtCore, QtGui, QtWidgets
-from .main_window import Ui_MainWindow
-
-
-class UpdateDialog(QtWidgets.QMainWindow, Ui_MainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setupUi(self)
-        self.ui = Ui_MainWindow()
